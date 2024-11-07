641ae42cf2c6f6482ad4c9de4a93cbcad568d5ac
lizaamatya <lizaamatya@gmail.com>
2024-11-06 20:41:36 -0600

    Train with bird dataset finetune controlnet

--- training_from_pretrained.py
+++ training_from_pretrained.py
@@ -110,14 +110,14 @@ def train_model(nn_model, data_loader, start_epoch, n_epoch):
                         # Upsample the spatial dimensions from [4, 1280, 4, 4] to [4, 1280, 256, 256]
                         nn.ConvTranspose2d(
                             in_channels=1280,
-                            out_channels=1280,  # Keep 1280 channels for now (no change)
+                            out_channels=640,  # Keep 1280 channels for now (no change)
                             kernel_size=4,  # Kernel size to upscale
                             stride=2,  # Stride of 2 to double spatial dimensions
                             padding=1,  # Ensure the spatial dimensions are doubled
                         ),
                         # Reduce the number of channels from 1280 to 3 (for RGB images)
                         nn.Conv2d(
-                            in_channels=1280,
+                            in_channels=640,
                             out_channels=3,  # Output channels: 3 (RGB)
                             kernel_size=1,  # Kernel size of 1 to reduce the channel count
                             stride=1,  # No change in spatial dimensions from this layer
