e205b205ab7018fa74029bd0a6d7eddc610e0c98
lizaamatya <lizaamatya@gmail.com>
2024-11-08 08:33:19 -0600

    Train with bird dataset finetune controlnet

--- training_from_pretrained.py
+++ training_from_pretrained.py
@@ -169,15 +169,15 @@ def train_model(nn_model, data_loader, start_epoch, n_epoch):
                 print(f"Epoch {ep+1}/{num_epochs}, Loss: {loss.item()}")
                 
             epoch_loss += loss.item()
-            # scaler.scale(loss).backward()
-            loss.backward()
+            scaler.scale(loss).backward()
+            # loss.backward()
             
             if (i + 1) % accumulation_steps == 0 or i == len(pbar):
-                # scaler.unscale_(optim)
-                # torch.nn.utils.clip_grad_norm_(nn_model.parameters(), max_norm=1.0)
-                optim.step()
-                # scaler.step(optim)
-                # scaler.update()
+                scaler.unscale_(optim)
+                torch.nn.utils.clip_grad_norm_(upsample_block.parameters(), max_norm=1.0)
+                # optim.step()
+                scaler.step(optim)
+                scaler.update()
                 optim.zero_grad(set_to_none=True)
         
             del (
